b625586b0fac697356fe4c6d7b15a64eb701bd43
Jenish Kunwar <jenishkunwar@Jenishs-MacBook-Air.local>
2025-11-12 11:54:12 +0545

    jenish kunwar

---
+++ Task 1/Operators.cs
@@ -0,0 +1,6 @@
+namespace Task_1;
+
+public class Operators
+{
+    
+}
---
+++ Task 5/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
