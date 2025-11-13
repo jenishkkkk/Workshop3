c959da1fa646ccce345b5755f48c659520a877d9
jenishkkkk <167462501+jenishkkkk@users.noreply.github.com>
2025-11-13 09:29:08 +0545

    Update Program.cs

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace Task_6;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered: " + number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number format");
+        }
+        finally
+        {
+            Console.WriteLine("Program Executed");
+        }
+
+        try
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            if (password.Length < 6)
+            {
+                throw new Exception("Password must be at least 6 characters.");
+            }
+
+            Console.WriteLine("Password satisfied the requirement");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
